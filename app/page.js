75adbf26541f68ef97b0f61e6ce9c5b46b975fd3
henachng <116464575+henachng@users.noreply.github.com>
2023-10-14 15:27:41 -0700

    implemented spotify authorization

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 
 export default function Home() {
+  const CLIENT_ID = "99439ec664294f8a84876ac4d38952f8"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "http://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
   return (
     <nav>
       <div>
@@ -13,6 +17,7 @@ export default function Home() {
             <a href="/photos">Photos</a>
           </li>
         </ul>
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&${RESPO}`}>SPOTIFY LOGIN</a>
       </div>
     </nav> 
   )
