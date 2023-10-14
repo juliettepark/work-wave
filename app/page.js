287d9b684f0a1ec9b202da63dd28cbd0e627cd6f
juliettepark <69448181+juliettepark@users.noreply.github.com>
2023-10-14 16:46:26 -0700

    yeet

--- app/page.js
+++ app/page.js
@@ -15,10 +15,7 @@ function Navbar() {
     <nav className="border-b border-gray-800 sticky top-0 bg-gray-900 text-gray-100 z-10">
       <div className="h-14 max-w-7xl p-4 mx-auto flex items-center justify-between">
         <a href="/" className='font-medium text-lg md:hover:underline'>Home</a>
-        <ul className='flex items-center justify-end space-x-4 text-sm font-medium'>
-          <li className='md:hover:underline'>
-            <a href="/login.js">Login</a>
-          </li>
+        <ul className='flex items-center justify-end space-x-4 text-sm font-medium'> 
           <li className='md:hover:underline'>
             <a href="/about.js">About</a>
           </li>
